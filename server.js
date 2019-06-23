b26ac0c691baeb2b30a28ed99a39156039a2bc4d
Kahr-Noss <maxime.collodel@orange.fr>
2019-06-24 01:09:25 +0900

    add welcome messages

--- server.js
+++ server.js
@@ -34,10 +34,88 @@ bot.on('message', function (event) {
   }
 });
 
+bot.on('follow', function (event) {
+  const answerEn = `Hello!!
+I'm a Magic the Gathering card fetcher bot. Just ask me any card name and I'll show it for you.
+You can add me to a LINE discussion group about Magic, and I'll make your conversation easier to understant for everyone by showing the card you are talking about.
+No more need to copy paste the card by yourself.
+
+Just write the name of a card in double parenthesis like this :
+((sol ring))
+
+If you want a special set version, you can specify it's code (usually 3 letters) it with @ :
+((sol ring @ alpha))
+
+You can also specify multiple cards in one message, and mix them in setences:
+
+"Did you see the combo with ((kiki jiki)) and ((zealous conscripts)) ? That's nuts."
+
+I work the best in English, where I can match a card even if you don't write the full name as long as it's not ambiguous.
+I also can search in any other language, but the name has to be the exact one.
+
+If I match more than one card or no card, I won't answer anything, so check if your input is correct.
+
+Also, during spoiler season, I'll notify you with the last spoiled card as they are revealed.
+
+I hope I'll be helpful to you! `
+  event.reply(answerEn);
+
+  const data = JSON.parse(fs.readFileSync('./data/stats.json'));
+  fs.writeFileSync('./data/stats.json', JSON.stringify({
+    daily: {
+      ...data.daily,
+      friends: data.daily.friends + 1
+    },
+    general: {
+      ...data.general,
+      friends: data.general.friends + 1
+    }
+  }, null, 2));
+});
+
+bot.on('join', function (event) {
+  const answerEn = `Hello!!
+I'm a Magic the Gathering card fetcher bot. Just ask me any card name and I'll show it for you.
+You can add me to a LINE discussion group about Magic, and I'll make your conversation easier to understant for everyone by showing the card you are talking about.
+No more need to copy paste the card by yourself.
+
+Just write the name of a card in double parenthesis like this :
+((sol ring))
+
+If you want a special set version, you can specify it's code (usually 3 letters) it with @ :
+((sol ring @ alpha))
+
+You can also specify multiple cards in one message, and mix them in setences:
+
+"Did you see the combo with ((kiki jiki)) and ((zealous conscripts)) ? That's nuts."
+
+I work the best in English, where I can match a card even if you don't write the full name as long as it's not ambiguous.
+I also can search in any other language, but the name has to be the exact one.
+
+If I match more than one card or no card, I won't answer anything, so check if your input is correct.
+
+Also, during spoiler season, I'll notify you with the last spoiled card as they are revealed.
+
+I hope I'll be helpful to you! `
+  event.reply(answerEn);
+
+  const data = JSON.parse(fs.readFileSync('./data/stats.json'));
+  fs.writeFileSync('./data/stats.json', JSON.stringify({
+    daily: {
+      ...data.daily,
+      groups: data.daily.groups + 1
+    },
+    general: {
+      ...data.general,
+      groups: data.general.groups + 1
+    }
+  }, null, 2));
+});
+
 // check the spoilers every 5 min
 cron.schedule('*/5 * * * *', () => {
   console.log('Checking new spoilers...')
-  getSpoilersUtil('m20')
+  getSpoilersUtil()
     .then((messageList) => {
       console.log(messageList);
       if (process.env.ENVIRONMENT === 'production') {
@@ -53,6 +131,27 @@ cron.schedule('*/5 * * * *', () => {
 });
 
 
+// send stats every morning and reset stats
+cron.schedule('0 0 * * *', () => {
+  const data = JSON.parse(fs.readFileSync('./data/stats.json'));
+  const msg = `Here are today stats:
+  New friends: ${data.daily.friends} (${data.general.friends})
+  New groups: ${data.daily.groups} (${data.general.groups})
+  calls: ${data.daily.calls} (${data.general.calls})
+  matchs: ${data.daily.matchs} (${data.general.matchs})`;
+
+  fs.writeFileSync('./data/stats.json', JSON.stringify({
+    ...data,
+    daily: {
+      friends: 0,
+      groups: 0,
+      calls: 0,
+      matchs: 0
+    }
+  }, null, 2));
+  bot.push(process.env.TEST_LINE_ID, msg);
+});
+
 
 const linebotParser = bot.parser();
 app.post('/webhook', linebotParser);
@@ -66,7 +165,7 @@ if (process.env.ENVIRONMENT === 'test') {
     //     bot.push(process.env.TEST_LINE_ID, answer);
     //   });
 
-    // getSpoilersUtil('m20')
+    // getSpoilersUtil()
     //   .then((messageList) => {
     //     if (process.env.ENVIRONMENT === 'production') {
     //       bot.broadcast(messageList);
--- utils/get_spoilers_util.js
+++ utils/get_spoilers_util.js
@@ -4,25 +4,20 @@ const fs = require('fs');
 
 const { getCardMessage, compileCardMessages } = require('./create-card-message');
 
-function getLastSpoilers(set) {
-  if (!set) {
-    return Promise.reject();
-  }
-
+function getLastSpoilers() {
+  const data = JSON.parse(fs.readFileSync('./data/spoilers.json'));
   const search = {
-    q: `set:${set}`,
+    q: `set:${data.set}`,
     format: 'json',
     order: 'spoiled'
   }
-  const spoilerData = JSON.parse(fs.readFileSync('./data/spoilers.json'));
-  console.log(spoilerData);
   // get the cards from specified set in spoiled order
   return request.get(`https://api.scryfall.com/cards/search?${querystring.stringify(search)}`)
     .then((setDataJSON) => {
       const setData = JSON.parse(setDataJSON);
 
       // get the index of the last announced spoiled card
-      const lastIndex = setData.data.findIndex((card) => card.id === spoilerData.last_card_id);
+      const lastIndex = setData.data.findIndex((card) => card.id === data.last_card_id);
       if (lastIndex === -1) {
         return Promise.reject('last card not found');
       }
@@ -30,7 +25,10 @@ function getLastSpoilers(set) {
         return Promise.reject('No new card published');
       }
       // save the new last card ID
-      fs.writeFileSync('./data/spoilers.json', JSON.stringify({ ...spoilerData, last_card_id: setData.data[0].id }, null, 2));
+      fs.writeFileSync('./data/spoilers.json', JSON.stringify({
+        set: data.set,
+        last_card_id: setData.data[0].id
+      }, null, 2));
 
       // send the subarray of the new cards spoiled since last chack
       return Promise.all(setData.data.slice(0, lastIndex).map((card) => getCardMessage(card, true)));
