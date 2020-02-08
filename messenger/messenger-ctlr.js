e8459faa4f4d7bb2da4cdd9cece7c235587768af
Kahr-Noss <maxime.collodel.jp@gmail.com>
2020-02-08 19:12:33 +0900

    add messenger

--- messenger/messenger-ctlr.js
+++ messenger/messenger-ctlr.js
@@ -1,38 +1,9 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const axios = require('axios');
-// const {  FacebookMessagingAPIClient,  FacebookMessageParser,  ValidateWebhook} = require("fb-messenger-bot-api");
-const matchCardsUtil = require("../utils/match-cards-util");
+const matchCardsUtil = require('../utils/match-cards-util');
 
 const router = express.Router();
-// const messagingClient = new FacebookMessagingAPIClient(
-//   process.env.PAGE_ACCESS_TOKEN
-// );
-
-const privacyPolicy = (req, res) => {
-  console.log("Privacy policy");
-  res.sendFile(path.join(__dirname + "/privacyPolicy.html"));
-};
-
-// const getMessage = (req, res) => {
-//   console.log("PLOP MESG");
-
-//   const incomingMessages = FacebookMessageParser.parsePayload(req.body);
-//   console.log(incomingMessages);
-//   // the incommingmessages are an array
-//   const senderId = incomingMessages[0].sender.id;
-
-//   //promise based reaction on message send confirmation
-//   messagingClient.sendImageMessage(senderId,"https://img.scryfall.com/cards/large/front/a/4/a4227afa-780b-4755-9b61-46255717c6be.jpg?1579203420")
-//     .then(result => {
-//       console.log(`Result sent with: ${result}`);
-//       return res.sendStatus(200);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       return res.sendStatus(200);
-//     });
-// };
 
 const verifyWebhook = (req, res) => {
   const VERIFY_TOKEN = 'mtgcrdftchrtkn';
@@ -44,51 +15,54 @@ const verifyWebhook = (req, res) => {
   if (mode && token === VERIFY_TOKEN) {
     res.status(200).send(challenge);
   } else {
-      res.sendStatus(200);
-    }
+    res.sendStatus(200);
+  }
+};
+
+const privacyPolicy = (req, res) => {
+  console.log('Privacy policy');
+  res.sendFile(path.join(`${__dirname}/privacyPolicy.html`));
 };
 
 const getMessage = (req, res) => {
-const event = req.body.entry[0].messaging[0];
-const senderId = event.sender.id;
-const message = event.message.text;
+  const event = req.body.entry[0].messaging[0];
+  const senderId = event.sender.id;
+  const message = event.message.text;
 
-if(!message) {
-  return res.sendStatus(200);
-}
+  if (!message) {
+    return res.sendStatus(200);
+  }
 
-return matchCardsUtil(message)
-.then((cardList) => {
- return cardList.reduce((promiseChain, card) => {
-  return promiseChain
-  .then(() => axios.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}` ,{
-      "recipient":{
-        "id":senderId
+  return matchCardsUtil(message)
+    .then((cardList) => cardList.reduce((promiseChain, card) => promiseChain.then(() => axios.post(
+      `https://graph.facebook.com/v2.6/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}`,
+      {
+        recipient: {
+          id: senderId,
+        },
+        message: {
+          attachment: {
+            type: 'image',
+            payload: {
+              url: card.imageUrl,
+              is_reusable: true,
+            },
+          },
+        },
       },
-      "message":{
-        "attachment":{
-          "type":"image", 
-          "payload":{
-            "url":card.imageUrl, 
-            "is_reusable":true
-          }
-        }
-      }
+    )), Promise.resolve()))
+    .then(() => {
+      res.sendStatus(200);
     })
-  )
-  }, Promise.resolve())
-})
-.then(() => {
-  res.sendStatus(200);
-})
-.catch((err) => {
-  console.log(err);
-  return res.sendStatus(200);
-});
+    .catch((err) => {
+      console.log(err);
+      return res.sendStatus(200);
+    });
 };
 
-router.get("/webhook", verifyWebhook);
-// router.get("/webhook", (req, res) =>  ValidateWebhook.validateServer(req, res, "mtgcrdftchrtkn"));
-router.get("/privacy-policy", privacyPolicy);
-router.post(  "/webhook",  (req, res, next) => {    console.log("plop test");   next();  },  getMessage);
+router.get('/privacy-policy', privacyPolicy);
+router.get('/webhook', verifyWebhook);
+router.post('/webhook', getMessage);
+
+
 module.exports = router;
--- utils/match-cards-util.js
+++ utils/match-cards-util.js
@@ -1,36 +1,35 @@
 const request = require('request-promise');
-const querystring = require("querystring");
+const querystring = require('querystring');
 
 const { saveStats } = require('./save-stats');
 
 function parseCardObject(card) {
-      // get the two faces of the card if it's a double faced card
-      const cardFacesList = card.card_faces ? card.card_faces.filter((c) => c.image_uris) : [];
-      const cardList =[...(cardFacesList.length > 0 ? cardFacesList :[card])];
+  // get the two faces of the card if it's a double faced card
+  const cardFacesList = card.card_faces ? card.card_faces.filter((c) => c.image_uris) : [];
+  const cardList = [...(cardFacesList.length > 0 ? cardFacesList : [card])];
 
-      // if there are related cards, send them
- return Promise.all((card.all_parts || [])
+  // if there are related cards, send them
+  return Promise.all((card.all_parts || [])
   // remove present card, tokens and emblems from the list
     .filter((c) => c.id !== card.id && c.component !== 'token' && !c.type_line.match(/^Emblem/))
-    .map((c) => request.get(c.uri).then((relatedCardJSON) => JSON.parse(relatedCardJSON)))
-    )
-    .then((relatedCards) =>{
-    return  [...cardList, ...relatedCards].map((c) => ({
-      imageUrl:c.image_uris.normal,
-      name:c.name,
-      mana_cost:c.mana_cost,
-      type_line:c.type_line,
-      oracle_text:c.oracle_text,
-      power:c.power,
-      toughness:c.toughness,
-      loyalty:c.loyalty,          
-  }))});
+    .map((c) => request.get(c.uri).then((relatedCardJSON) => JSON.parse(relatedCardJSON))))
+    .then((relatedCards) => [...cardList, ...relatedCards].map((c) => ({
+      imageUrl: c.image_uris.normal,
+      name: c.name,
+      mana_cost: c.mana_cost,
+      type_line: c.type_line,
+      oracle_text: c.oracle_text,
+      power: c.power,
+      toughness: c.toughness,
+      loyalty: c.loyalty,
+      relatedCards: c.name === card.name ? null : card.name,
+    })));
 }
 
 
 function matchCards(message) {
   // check if the card name pattern has been inputed  (( card name ))
-  const nameList = message.match(/\(\([^\)]+\)\)/g);
+  const nameList = message.match(/\(\([^)]+\)\)/g);
   if (!nameList) {
     return Promise.reject('No pattern detected');
   }
@@ -47,22 +46,22 @@ function matchCards(message) {
       const search = {
         fuzzy: name.trim(),
         format: 'json',
-        set: set ? set.trim() : ''
+        set: set ? set.trim() : '',
       };
       return request.get(`https://api.scryfall.com/cards/named?${querystring.stringify(search)}`)
         .then((cardDataJSON) => {
           saveStats('matchs', 1);
           return parseCardObject(JSON.parse(cardDataJSON));
         })
-        .catch((err) => {
+        .catch(() => {
           console.log(`${name} => No unique card found, trying other languages`);
           // if nothing matched in english, try other languages
-          const search = {
+          const search2 = {
             q: `"${name.trim()}" ${set ? `set:${set}` : ''}`,
             include_multilingual: true,
-            format: 'json'
+            format: 'json',
           };
-          return request.get(`https://api.scryfall.com/cards/search?${querystring.stringify(search)}`)
+          return request.get(`https://api.scryfall.com/cards/search?${querystring.stringify(search2)}`)
             .then((resultJSON) => {
               const result = JSON.parse(resultJSON);
               if (result.total_cards === 1) {
@@ -71,7 +70,7 @@ function matchCards(message) {
               }
               throw new Error(`${result.total_cards} cards found in another language`);
             })
-            .catch((err) => {
+            .catch(() => {
               console.log('No unique card found in another language');
               return [];
             });
@@ -79,18 +78,18 @@ function matchCards(message) {
     }
     return [];
   }))
-  .then((CardArrayList) => {
-  const cardURLList = [];
-        return CardArrayList.reduce((buffer, cardArray) => [...buffer, ...cardArray], [])
-    // remove duplicates
-    .filter((c) => {
-      if (cardURLList.indexOf(c.imageUrl) === -1) {
-        cardURLList.push(c.imageUrl);
-        return true;
-      }
-      return false;
-    })
-  });
+    .then((CardArrayList) => {
+      const cardURLList = [];
+      return CardArrayList.reduce((buffer, cardArray) => [...buffer, ...cardArray], [])
+      // remove duplicates
+        .filter((c) => {
+          if (cardURLList.indexOf(c.imageUrl) === -1) {
+            cardURLList.push(c.imageUrl);
+            return true;
+          }
+          return false;
+        });
+    });
 }
 
 module.exports = matchCards;
